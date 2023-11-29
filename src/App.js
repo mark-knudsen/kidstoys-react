d2d242126bb931fed7e93a079611b7496cb3831d
jeff0167 <73894404+jeff0167@users.noreply.github.com>
2023-11-29 15:27:25 +0100

    className

--- src/App.js
+++ src/App.js
@@ -26,12 +26,12 @@ function App() {
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" onClick={() => navigate("kidstoys")}>Home</a>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" onClick={() => navigate("kidstoys")}>Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" onClick={() => navigate("kidstoys/products")}>Products</a>
+              <li className="nav-item">
+                <a className="nav-link" onNameClick={() => navigate("kidstoys/products")}>Products</a>
               </li>
               {/* <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -36,12 +36,12 @@ function FrontPage() {
 
 
     return (
-        <div class="container">
+        <div className="container">
             <br />
-            <div class="carousel-container text-center">
-                <div id="carouselExampleCaptions" class="carousel slide " data-bs-ride="carousel" >
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <div className="carousel-container text-center">
+                <div id="carouselExampleCaptions" className="carousel slide " data-bs-ride="carousel" >
+                    <div className="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                     </div>
@@ -76,16 +76,8 @@ function FrontPage() {
                 <br />
             </div>
 
-
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5">
                 {productContent}
-
-
-
-
-
-
-
                 {/*   <div className="col">
                     <div className="card card-style d-flex justify-content-center align-items-center">
                         <img src="../images/test/test-1.jpg" className="card-img-top img-fluid" alt="..."></img>
@@ -119,9 +111,7 @@ function FrontPage() {
 
             </div>
             <br /><br />
-
         </div>
-
     );
 }
 
