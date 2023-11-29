65f2a37d5adde4d95603629062c73b6e1c478c11
Mark Svendstrup Knudsen <mark4610@edu.easj.dk>
2023-11-29 12:33:14 +0100

    small

--- src/App.js
+++ src/App.js
@@ -16,24 +16,24 @@ function App() {
   return (
     <div className="App">
       <nav class="navbar sticky-top navbar-expand-lg navbar-light navbar-color">
-            <div class="container-fluid">
-                <img src="./kidstoys-logo.png" class="navbar-brand" href="/kidstoys/"/>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <form class="d-flex nav-search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/kidstoys/products">Products</a>
-                        </li>
-                        {/* <li class="nav-item dropdown">
+        <div class="container-fluid">
+          <img src="./kidstoys-logo.png" class="navbar-brand" href="/kidstoys/" />
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <form class="d-flex nav-search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/kidstoys">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/kidstoys/products">Products</a>
+              </li>
+              {/* <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Products
                             </a>
@@ -44,11 +44,11 @@ function App() {
                                 <li><a class="dropdown-item" href="#">Something else here</a></li>
                             </ul>
                         </li> */}
-                    </ul>
-                    <i class="bi bi-cart3"></i>
-                </div>
-            </div>
-        </nav>
+            </ul>
+            <i class="bi bi-cart3"></i>
+          </div>
+        </div>
+      </nav>
 
 
       {/* 
